305cf2bf1537e44ef569c1d0e863ed128c327c6e
DawidKlejc <klejcd@gmail.com>
2022-04-07 22:29:46 -0400

    migrated manifest to v3

--- content.js
+++ content.js
@@ -1,3 +1,31 @@
+function retrieveISQ_Data() {
+    return new Promise((resolve, reject) => {
+        let ISQData = [];
+
+        // Used to split ISQ Data into a 2D-Array, where each outer index consists of an array of 2 elements: Course & Professor Name 
+        function stringTo2dArray(string, d1, d2) {
+            return string.split(d1).map(function(x){return x.split(d2)});
+        }
+
+        let requestISQData = new XMLHttpRequest();
+        requestISQData.open('GET', 'https://gist.githubusercontent.com/DavidKlejc/0e339bdad700eea6c70fff2c14cbff32/raw/af60480b53bfcfd79bef6650073c1358d8585263/ISQData.csv', true);
+        requestISQData.onreadystatechange = function()
+        {
+            if(requestISQData.readyState == XMLHttpRequest.DONE && requestISQData.status == 200)
+            {
+                ISQData = stringTo2dArray(requestISQData.response, "\n", "\t");
+                for(let i = 0; i < ISQData.length; i++) {
+                    for(let j = 0; j < 1; j++) {
+                        ISQData[i][0] = ISQData[i][0].replace(",", ""); 
+                    }
+                }
+                resolve(ISQData);
+            }
+        }
+        requestISQData.send();
+    });
+}
+
 window.addEventListener('load', () => {
     if (window.location.href == 'https://banregister.unf.edu/StudentRegistrationSsb/ssb/classSearch/classSearch'){
 
@@ -39,34 +67,6 @@ window.addEventListener('load', () => {
                             resolve(courseAndProfessorName);
                         }, 3500); 
                     }
-            });
-        }
-
-        function retrieveISQ_Data() {
-            return new Promise((resolve, reject) => {
-                let ISQData = [];
-
-                // Used to split ISQ Data into a 2D-Array, where each outer index consists of an array of 2 elements: Course & Professor Name 
-                function stringTo2dArray(string, d1, d2) {
-                    return string.split(d1).map(function(x){return x.split(d2)});
-                }
-
-                let requestISQData = new XMLHttpRequest();
-                requestISQData.open('GET', 'https://gist.githubusercontent.com/DavidKlejc/0e339bdad700eea6c70fff2c14cbff32/raw/af60480b53bfcfd79bef6650073c1358d8585263/ISQData.csv', true);
-                requestISQData.onreadystatechange = function()
-                {
-                    if(requestISQData.readyState == XMLHttpRequest.DONE && requestISQData.status == 200)
-                    {
-                        ISQData = stringTo2dArray(requestISQData.response, "\n", "\t");
-                        for(let i = 0; i < ISQData.length; i++) {
-                            for(let j = 0; j < 1; j++) {
-                                ISQData[i][0] = ISQData[i][0].replace(",", ""); 
-                            }
-                        }
-                        resolve(ISQData);
-                    }
-                }
-                requestISQData.send();
             });
         }
 
