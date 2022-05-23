e0287320665c4beef56baf7c29bbe37509ff5500
DawidKlejc <klejcd@gmail.com>
2022-05-23 04:42:38 -0400

    Complete refactor to use new professor rating API

--- content.js
+++ content.js
@@ -1,113 +1,87 @@
 window.addEventListener('load', () => {
-    if (window.location.href == 'https://banregister.unf.edu/StudentRegistrationSsb/ssb/classSearch/classSearch'){
+    if (window.location.href == 'https://banregister.unf.edu/StudentRegistrationSsb/ssb/classSearch/classSearch') {
+        
+    var searchCounter = 0;
+    var professorRating = 0;
 
-        var entireTable;
-        var searchCounter = 0;
-
-        function retrieveCourseAndProfessorName() {
-            return new Promise((resolve, reject) => {
-                    let searchButton = document.getElementById("search-go");
-                    searchButton.onclick = classPageLoaded;
-
-                    function classPageLoaded() {
-                        // Changes the amount of courses displayed in the table from 10 per page to 50 per page
+        function addNewRatingColumnOnSearch() {
+            return new Promise((resolve) => {
+                if(searchCounter < 1) {
+                    const searchButton = document.getElementById("search-go");
+                    searchButton.onclick = () => {
                         setTimeout(() => {
-                            const selections = document.querySelector('select.page-size-select');
-                            selections.value = 50;
-                            const evt = document.createEvent("HTMLEvents");
-                            evt.initEvent("change", true, true);
-                            selections.dispatchEvent(evt);
-                        }, 1000);
-
-                        setTimeout(() => {  
-                            let courseFromCoursePage;
-                            let courseAndProfessorName = [];   
-                            let subject = document.querySelector("#searchTerms > span:nth-child(2) > label > span").innerHTML.substring(0, 3);
-                            const trlist = document.querySelector("#table1 > tbody").getElementsByTagName("tr");
-                            entireTable = Array.from(trlist); 
-
-                            for(let i = 0; i < entireTable.length; i++) {
-                                let currentProfessor = Array.from(entireTable[i].cells[7].getElementsByTagName("a"));
-                                if(currentProfessor != "") {
-                                    let professorLastNameFromCoursePage = currentProfessor[0].innerHTML.split(" ").pop();;
-                                    courseFromCoursePage = subject.concat(entireTable[i].cells[2].innerHTML.toString());
-                                    courseAndProfessorName[i] = courseFromCoursePage.concat(" ", professorLastNameFromCoursePage); 
-                                } else {
-                                    courseAndProfessorName[i] = courseFromCoursePage.concat(" ", "No professor listed");
-                                }
-                            }
-                            resolve(courseAndProfessorName);
-                        }, 3500); 
+                            const columnToClone = document.querySelector("#table1 > thead > tr > th.sort-disabled.linked-col.ui-state-default");
+                            let clone = columnToClone.cloneNode(true);
+                            clone.innerHTML = "Rating";
+                            clone.style.textAlign = "left";
+                            let header = document.querySelector("#table1 > thead > tr");
+                            header.appendChild(clone);
+                            searchCounter++;
+                            resolve();
+                        }, 1200);
                     }
+                } else {
+                    setTimeout(() => {
+                        resolve();
+                    }, 1200);
+                }
             });
         }
 
-        function retrieveISQ_Data() {
-            return new Promise((resolve, reject) => {
-                let ISQData = [];
+        function newPageLoad() {
+            return new Promise((resolve) => {
+                const searchAgainButton = document.getElementById("search-again-button");
+                const firstButton = document.querySelector("[title='First']");
+                const previousButton = document.querySelector("[title='Previous']");
+                const nextButton = document.querySelector("[title='Next']");
+                const lastButton = document.querySelector("[title='Last']");
 
-                // Used to split ISQ Data into a 2D-Array, where each outer index consists of an array of 2 elements: Course & Professor Name 
-                function stringTo2dArray(string, d1, d2) {
-                    return string.split(d1).map(function(x){return x.split(d2)});
+                searchAgainButton.onclick = () => {
+                    const searchButton = document.getElementById("search-go");
+                    searchButton.onclick = () => resolve();
                 }
+                firstButton.onclick = () => resolve();
+                previousButton.onclick = () => resolve();
+                nextButton.onclick = () => resolve();
+                lastButton.onclick = () => resolve();
+            });
+        }
 
-                let requestISQData = new XMLHttpRequest();
-                requestISQData.open('GET', 'https://gist.githubusercontent.com/DavidKlejc/0e339bdad700eea6c70fff2c14cbff32/raw/af60480b53bfcfd79bef6650073c1358d8585263/ISQData.csv', true);
-                requestISQData.onreadystatechange = function()
-                {
-                    if(requestISQData.readyState == XMLHttpRequest.DONE && requestISQData.status == 200)
-                    {
-                        ISQData = stringTo2dArray(requestISQData.response, "\n", "\t");
-                        for(let i = 0; i < ISQData.length; i++) {
-                            for(let j = 0; j < 1; j++) {
-                                ISQData[i][0] = ISQData[i][0].replace(",", ""); 
-                            }
-                        }
-                        resolve(ISQData);
-                    }
+        function insertRatingIntoRegistrationPage() {
+            return new Promise((resolve) => {
+                const subject = document.querySelector("#searchTerms > span:nth-child(2) > label > span").innerHTML.substring(0, 3);
+                const trlist = document.querySelector("#table1 > tbody").getElementsByTagName("tr");
+                let entireTable = Array.from(trlist);
+    
+                for(let i = 0; i < entireTable.length; i++) {
+                    let currentProfessor = Array.from(entireTable[i].cells[7].getElementsByTagName("a"));
+                    let professorLastNameFromCoursePage = currentProfessor[0].innerHTML.split(" ").pop();
+                    let courseFromCoursePage = subject.concat(entireTable[i].cells[2].innerHTML.toString());
+
+                    fetch(`https://professor-rating-api.herokuapp.com/${courseFromCoursePage}/${professorLastNameFromCoursePage}`)
+                    .then(response => response.json())
+                    .then(json => {
+                        professorRating = json[0].Rating;
+                        entireTable[i].insertCell(14);
+                        const ratingForTable = document.createTextNode(professorRating);
+                        entireTable[i].cells[14].appendChild(ratingForTable);
+                    }).catch(err => {
+                        professorRating = 'No Rating Found';
+                        entireTable[i].insertCell(14);
+                        const ratingForTable = document.createTextNode(professorRating);
+                        entireTable[i].cells[14].appendChild(ratingForTable);
+                    });
                 }
-                requestISQData.send();
+                resolve();
             });
         }
 
         async function insertIntoTable() {
-
-            const coursesAndProfessors = await retrieveCourseAndProfessorName();
-            const ISQ_Data = await retrieveISQ_Data();
-
-            let tableOfRatings = [];
-
-            for(let i = 0; i < coursesAndProfessors.length; i++) {
-                let professorRating = "No rating found";
-                for(let j = 0; j < ISQ_Data.length; j++) {
-                    if(coursesAndProfessors[i] === ISQ_Data[j][0]) {
-                        professorRating = parseFloat(ISQ_Data[j][1]);
-                        professorRating = professorRating.toFixed(2);
-                    }
-                }
-                tableOfRatings[i] = professorRating;
-            }
-
-            if(searchCounter < 1) {
-                let columnToClone = document.querySelector("#table1 > thead > tr > th.sort-disabled.linked-col.ui-state-default");
-                let clone = columnToClone.cloneNode(true);
-                clone.innerHTML = "Rating";
-                clone.style.textAlign = "left";
-                let header = document.querySelector("#table1 > thead > tr");
-                header.appendChild(clone);
-                searchCounter++;
-            }
-
-            for(let i = 0; i < tableOfRatings.length; i++) {
-                entireTable[i].insertCell(14);
-                let ratingForTable = document.createTextNode(tableOfRatings[i]);
-                entireTable[i].cells[14].appendChild(ratingForTable);
-            }
-
-            let searchAgainButton = document.getElementById("search-again-button");
-            searchAgainButton.onclick = insertIntoTable;
-
+            await addNewRatingColumnOnSearch();
+            await insertRatingIntoRegistrationPage();
+            await newPageLoad();
+            insertIntoTable();
         }
         insertIntoTable();
-    } 
+    }
 }); 
